fc02452f6c46fd2ce90d1ccc53e862e69e968bb5
Yandi Vargas <yandivd@gmail.com>
2024-12-15 23:38:30 -0500

    pass not serializer

--- torneos/api/serializers.py
+++ torneos/api/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        exclude = ['password']
 
 class PlayerSerializer(serializers.ModelSerializer):
     user = UserSerializer()
